4c486ee4b08945310b55a3e0659fb55314a76d5e
Liora Laulier <liora@permettezmoideconstruire.fr>
2021-07-09 17:50:46 +0200

    factorial example

---
+++ exercices/factorial.js
@@ -0,0 +1,22 @@
+// function factorial(x) {
+//     const result = 1;
+//     for (let i = 1; i <= x; i++)
+//       result = result * i;
+//     return result;
+//   }
+//   function recursivefactorial(x) {
+//     // This is the base case.
+//     if (x === 0) return 1;
+//     // This is the recursive one.
+//     else return x * factorial(x - 1);
+//   }
+  
+  // factorial(5); // output : 120
+
+  // Le cas de base représente l'ensemble des lignes de code qui permet l'arrêt de la récursion. 
+  // Habituellement, cet ensemble débute avec une clause conditionnelle comme if. 
+  // Si cette notion n'est pas mise en place, la fonction sera répétée de manière infinie et le programme plantera. 
+  // Le cas de base est donc la condition de fin de la fonction récursive.
+
+
+  
