ff1b5e77dcb1b38689e99b3aab6fd550946c0e73
Liora Laulier <liora@permettezmoideconstruire.fr>
2021-07-13 17:16:54 +0200

    Uber array result  with divison use

--- exercices/codeProblem2.js
+++ exercices/codeProblem2.js
@@ -1,4 +1,27 @@
+// #HARD
 // This problem was asked by Uber.
 // Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
 // For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 // Follow-up: what if you can't use division?
+// const inputArray = [1,2,3,4,5];
+
+function getProductofArray(inputArray){
+    let P = []
+    let l = inputArray.length
+    for (let j = 0 ; j < l; j++ ){ 
+        let product = 1;
+        for (let i = 0 ; i < l; i++ ){
+        if(j === i){
+            P[j] = product/inputArray[j]
+        } else {
+            product = product * inputArray[i]
+            
+        }
+        P[j] = product
+        
+    }
+    console.log(P[j]) 
+}
+}
+
+module.exports = getProductofArray
--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const getWordPatternIsomorphic = require('./exercices/wordPattern.js');
 const getCakes = require('./exercices/bakerRecipes.js');
 const getMumbling = require('./exercices/mumbling');
 const getAddUpToK = require('./exercices/codeProblem1');
+const getProductofArray = require('./exercices/codeProblem2');
 
-const result1 = generateHashtag(' To big');
-console.log(`result of hastag: ${result1}`);
+// const result1 = generateHashtag(' To big');
+// console.log(`result of hastag: ${result1}`);
 
-const result2 = getRomanNumerals('MMCX');
-console.log(`result of roman numerals: ${result2}`);
+// const result2 = getRomanNumerals('MMCX');
+// console.log(`result of roman numerals: ${result2}`);
 
-const result3 = getWordPatternIsomorphic('acca', "toque toque toque toque");
-console.log(`result of Isomorphism: ${result3}`);
+// const result3 = getWordPatternIsomorphic('acca', "toque toque toque toque");
+// console.log(`result of Isomorphism: ${result3}`);
 
-const cakeResult = getCakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
-console.log(`result of get Cakes: ${cakeResult}`);
+// const cakeResult = getCakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+// console.log(`result of get Cakes: ${cakeResult}`);
 
-const mumblingResult = getMumbling("cwAt")
-console.log(`result of Mumbling: ${mumblingResult}`);
+// const mumblingResult = getMumbling("cwAt")
+// console.log(`result of Mumbling: ${mumblingResult}`);
 
-const kResult = getAddUpToK([10, 15, 3, 7, 25], 13)
-console.log(`result of k: ${kResult}`);
+// const kResult = getAddUpToK([10, 15, 3, 7, 25], 13)
+// console.log(`result of k: ${kResult}`);
+const productResult = getProductofArray([4,3,2,1])
+console.log(`result of product array: ${productResult}`);
