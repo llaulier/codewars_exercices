94a9623f73b4b486dc20132db0643f1bd2407ec0
Liora Laulier <lioralaulier@mbp-de-liora-1.home>
2022-05-19 10:10:44 +0200

    parse regex modified

--- exercices/adressParcer.js
+++ exercices/adressParcer.js
@@ -1,5 +1,6 @@
 const addressTexts = [
     `89 rue des oies BP 4560 59160 Lomme`,
+    `23b chemin des allouettes lieu-dit "Rerandiere" 85170 SAINT-DENIS-LA-CHEVASSE`
     `13 rue des canards verts résidence "les pins" CS455OO 60530 Crouy-en-Thelle`
     `95 bis avenue du port Chez Mmme Michèle Michu 14000 CAEN`,
     `45 allée des acacias "la petite prairie" 80000 Amiens`,
@@ -7,6 +8,6 @@ const addressTexts = [
     `7 rue des anciens combattants et de la résistance 8233 Appartement 8 95120 Cergy`
   ];
   
-  const parseAddress = addressText => /(?<addressnum>\d+)\s(?<address1>[a-zA-ZÂŒÉÈÊËÇèçéîÎêëàù’'ëâôœ ]{1,33}\s\b)(?<adress2>[a-zA-ZÂŒÉÈÊËÇèçéîÎêëàù’'ëâôœ "1-9]{1,}\D\b)(?<zip>\d{5})\s(?<city>[a-zA-ZÂŒÉÈÊËŸÿÇûèçôÔÎîéêëàù/’'"ëâôœ -]*)/gm.exec(addressText).groups;
+  const parseAddress = addressText => /(?<addressnum>\d+|\d+[b|bis|ter])\s(?<address1>[a-zA-ZÂŒÉÈÊËÇèçéîÎêëàù’'ëâôœ -]{1,33}\s\b)(?<adress2>[a-zA-ZÂŒÉÈÊËÇèçéîÎêëàù’'ëâôœ "1-9-]{1,}\D\b)(?<zip>\d{5})\s(?<city>[a-zA-ZÂŒÉÈÊËŸÿÇûèçôÔÎîéêëàù/’'"ëâôœ -]*)/gm.exec(addressText).groups;
   
   addressTexts.forEach(ele => console.log(parseAddress(ele)));
